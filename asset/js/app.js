62f33b6e1faaa06fb8304b64d0ee00af1611f07a
Renz Michael <gallardo.renz8@gmail.com>
2023-07-17 00:43:53 +0800

    Added intialize files

---
+++ asset/js/app.js
@@ -0,0 +1,29 @@
+const scrollUp = document.querySelector("#scroll-up");
+
+const navLinks = document.querySelector("#navLinks");
+
+
+function navClose(){
+   navLinks.style.right = "-250px"
+}
+function navOpen(){
+    navLinks.style.right = "0"
+}
+
+
+
+
+
+
+// scrollUp
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+
+
+  
